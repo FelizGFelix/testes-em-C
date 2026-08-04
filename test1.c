c228e7b1f18e974bb1703d21e8ab2be4e7d21f35
Felipe Felix <lipegfelix@gmail.com>
2026-08-04 14:35:27 -0300

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world!\n");
+    printf("meu primeiro teste em C para a faculdade!\n");
+
+    int sixseven = 67;
+
+    printf("%d", sixseven);
+}
+
