1f8e52e0d3d487e2018684363bb461844ff91bbd
Felipe Felix <lipegfelix@gmail.com>
2026-08-06 14:21:52 -0300

    Add files via upload

---
+++ teste_trabalho.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int cadastrar(){
+    printf("teste bem concluído!");
+}
+
+int main(){
+    int escolha = 0;
+
+    printf("Bem-vindo ao sistema de cadastro de funcionários da Hells Market!\n");
+    printf("Escolha uma das opções: \n");
+    scanf("%d", &escolha);
+
+
+    if (escolha == 1){
+        cadastrar();
+    }
+}
