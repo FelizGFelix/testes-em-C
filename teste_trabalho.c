5445c23a22e7b49dcbf8992c622017a09f358726
Felipe Felix <lipegfelix@gmail.com>
2026-08-10 01:32:28 -0300

    Add files via upload

--- teste_trabalho.c
+++ teste_trabalho.c
@@ -5,6 +5,13 @@
 #include <string.h>
 #include <time.h>
 
+/* Definição do contador*/
+int contador_id = 0;
+
+void incrementar_contador(){
+    contador_id ++;
+}
+
 void limpar() {
     #ifdef _WIN32
         system("cls");
@@ -16,56 +23,66 @@ void limpar() {
 void cadastrar(){
     limpar();
     setlocale(LC_ALL, "Portuguese");
+    
+    FILE *arquivo = fopen("funcionarios.txt", "w");
 
     struct ficha_funcionario{
-        int cod_id;
+        int id;
         char nome[100];
-        char cpf[11];
+        char cpf[14];
         float credito;
         char endereco[500];
         char cep[8];
         char cidade[20];
-        char estado[20];
+        char estado[3];
     };
 
-    struct ficha_funcionario codigo_identificador;
-    struct ficha_funcionario nome_funcionario;
-    struct ficha_funcionario cpf_funcionario;
-    struct ficha_funcionario credito_funcionario;
-    struct ficha_funcionario endereco_funcionario;
-    struct ficha_funcionario cep_funcionario;
-    struct ficha_funcionario cidade_funcionario;
-    struct ficha_funcionario estado_funcionario;
-    
+    struct ficha_funcionario funcionario;
     
     char retornar[]= "";
 
-    printf("Bem-Vindo ao sistema de cadastro da Hells Market!\n");
+    char credito_temporario[500] = "";
+
+    printf("Sistema de cadastro\n");
     printf("Preencha o campo de informações: \n");
     printf("Digite o seu nome: \n");
     scanf("%*c");
-    fgets(nome_funcionario.nome, sizeof(nome_funcionario.nome), stdin);
+    fgets(funcionario.nome, sizeof(funcionario.nome), stdin);
     printf("Digite o seu CPF: \n");
-    scanf("%s", cpf_funcionario.cpf);
+    scanf("%s", funcionario.cpf, stdin);
     printf("Digite o seu limite de crédito: \n");
-    scanf("%f", &credito_funcionario.credito);
+    scanf("%*c");
+    fgets(credito_temporario, sizeof(credito_temporario), stdin);
     printf("Digite o seu endereço: \n");
     scanf("%*c");
-    fgets(endereco_funcionario.endereco, sizeof(endereco_funcionario.endereco), stdin);
+    fgets(funcionario.endereco, sizeof(funcionario.endereco), stdin);
     printf("Digite o seu cep: \n");
-    scanf("%s", cep_funcionario.cep);
+    scanf("%s", funcionario.cep);
     printf("Digite a sua cidade: \n");
     scanf("%*c");
-    fgets(cidade_funcionario.cidade, sizeof(codigo_identificador.cidade), stdin);
+    fgets(funcionario.cidade, sizeof(funcionario.cidade), stdin);
     printf("Digite o estado em que mora: \n");
     scanf("%*c");
-    fgets(estado_funcionario.estado, sizeof(estado_funcionario.estado), stdin);
+    fgets(funcionario.estado, sizeof(funcionario.estado),stdin);
 
-    codigo_identificador.cod_id = rand()%100;
+    funcionario.credito = atof(credito_temporario);
 
+    incrementar_contador();
+    funcionario.id = contador_id;
+
+    fprintf(arquivo, "%d,", funcionario.id);
+    fprintf(arquivo, "%s,", funcionario.nome);
+    fprintf(arquivo, "%s,", funcionario.cpf);
+    fprintf(arquivo, "%f,", funcionario.credito);
+    fprintf(arquivo, "%s,", funcionario.endereco);
+    fprintf(arquivo, "%s,", funcionario.cep);
+    fprintf(arquivo, "%s,", funcionario.cidade);
+    fprintf(arquivo, "%s", funcionario.estado);
+
+
+    fclose(arquivo);
 
     printf("Cadastro concluído!\n");
-    printf("Seu número identificador é de: %d\n", codigo_identificador.cod_id);
     printf("Digite qualquer valor para retornar: ");
     scanf("%s", retornar);
     limpar();
