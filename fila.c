00740f18401fafd1f3e9c41cf90d4f61b93505bd
Felipe Felix <lipegfelix@gmail.com>
2026-08-12 14:48:19 -0300

    teste de fila em c

---
+++ fila.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define _MAX 8
+#include <stdbool.h>
+
+
+int Fila[_MAX];
+int tam=0;
+char retornar;
+
+void limpar() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void Retira(){
+    limpar();
+    int elm = Fila[0], i;
+    for (i=0; i<tam; i++){
+        Fila[i]=Fila[i+1];
+    }
+    tam--;
+
+    printf("Digite qualquer valor para retornar: ");
+    scanf("%c", &retornar);
+    limpar();
+}
+
+void inserir(){
+    int elm;
+    printf("Digite um valor: \n");
+    scanf("%d", &elm);
+    Fila[tam] = elm;
+    tam ++;
+
+    if (tam == _MAX){
+        printf("Fila cheia");
+    }
+
+    else {
+        printf("%d adicionado com sucesso!\n", elm);
+    }
+
+    printf("Digite qualquer valor para retornar: ");
+    scanf("%c", &retornar);
+}
+
+void exibir(){
+    limpar();
+    int i;
+    for (i=0;i<tam;i++){
+        printf("%d, \n", Fila[i]);
+        i++;
+    }
+
+}
+
+
+int main (){
+    int escolha;
+
+    while(true){
+    
+        printf("Escolha uma das opções: \n1- inserir valores\n2- retirar valores\n3- exibir valores\n->");
+        scanf("%d", &escolha);
+
+        if (escolha == 1){
+            inserir();
+        }   
+
+        else if (escolha == 2){
+            Retira();
+        }
+
+        else{
+            exibir();
+        }
+
+    }
+
+    return 0;
+}
